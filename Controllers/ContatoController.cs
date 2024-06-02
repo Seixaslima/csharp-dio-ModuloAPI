5e91fca1e3831e038081cd054b0e2ed3d19e8636
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-06-02 13:45:53 -0300

    Criando endpoint Obter por id e atualizar

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -25,5 +25,33 @@ namespace csharp_dio_ModuloAPI.Controllers
             _context.SaveChanges();
             return Ok(contato);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            Contato contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return NotFound();
+
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
